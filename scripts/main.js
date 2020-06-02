28354b1765159254f27672b5f547bafa228b6c90
albertki <albertki98@gmail.com>
2020-06-02 17:04:33 -0400

    contactme mr, bottomborder on navbar scroll

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,22 @@
 $(function () {
   $(document).scroll(function () {
     var $nav = $(".fixed-top");
-    $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    $nav.toggleClass(['scrolled'], $(window).scrollTop() > 100);
+    if ($(window).scrollTop() > 100){
+      $('.fixed-top').addClass( "navbar-light");
+      $('.fixed-top').removeClass( "navbar-dark");
+    }
+    else {
+      $('.fixed-top').addClass( "navbar-dark");
+      $('.fixed-top').removeClass( "navbar-light");
+    }
+    // if ($(this).scrollTop() > $nav.height()) {
+    //   $nav.removeClass('navbar-dark').addClass('navbar-light')
+    // }
+    // else {
+    //   $nav.removeClass('navbar-light').addClass('navbar-dark')
+    // }
+    // $nav.toggleClass(['scrolled', 'navbar-light'], $(this).scrollTop() > $nav.height());
+
   });
 });
